8ab1d427a99eabbb55784b5df9147d256a49e09f
Lloyd Dewolf <lloydde.github@thedewolfs.com>
2020-01-28 12:26:33 -0800

    App customizations in a apps/ dir
    * flycut.sh

---
+++ apps/flycut.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Flycut
+###############################################################################
+
+# Switch to black scissors in menubar
+defaults write com.generalarcade.flycut menuIcon -int 3
